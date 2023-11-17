a322c38b971bbdeba9610e8b42cfc379f4885a3c
SamBigham <sambigham3@gmail.com>
2023-11-16 23:17:19 -0600

    committing code

---
+++ src/BrailleASCIITables.java
@@ -0,0 +1,12 @@
+public class BrailleASCIITables {
+    
+    public String toBraille(char letter){
+        return "str";
+    }
+    public String toASCII(String bits){
+        return "strASC";
+    }
+    public String toUnicode(String bits){
+        return "strUni";
+    }
+}
